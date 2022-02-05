bb487ad242ca216d383afe4074e8fa216701cd79
LegionModding <legion@legionmodding.com>
2022-02-05 18:25:43 +0000

    Initial commit, blank mod

---
+++ src/main/java/com/legionmodding/openblocksrenewed/OpenBlocksRenewed.java
@@ -0,0 +1,37 @@
+package com.legionmodding.openblocksrenewed;
+
+import com.legionmodding.openblocksrenewed.util.Reference;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(Reference.MOD_ID)
+public class OpenBlocksRenewed
+{
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public OpenBlocksRenewed()
+    {
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientSetup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onCommonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void onClientSetup(FMLClientSetupEvent event)
+    {
+
+    }
+
+    private void onCommonSetup(FMLCommonSetupEvent event)
+    {
+
+    }
+}
---
+++ src/main/java/com/legionmodding/openblocksrenewed/util/Reference.java
@@ -0,0 +1,11 @@
+package com.legionmodding.openblocksrenewed.util;
+
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+
+public class Reference
+{
+    public static final String MOD_ID = "openblocksrenewed";
+
+    public static final String VERSION = "0.0.1";
+}
